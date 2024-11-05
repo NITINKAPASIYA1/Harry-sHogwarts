0f25f5a3f39e8a6ffb94344a72252b432551fcd7
Nitin <kumarnitin5814@gmail.com>
2024-11-06 00:21:25 +0530

    Initial Commit

---
+++ HogwartsHarry/HogwartsHarryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HogwartsHarryApp.swift
+//  HogwartsHarry
+//
+//  Created by Nitin ‘s on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HogwartsHarryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
